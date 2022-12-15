84499ed96fe9975b91102bedfbca6694aa9f4d11
Nikita <118795537+W1nkobot@users.noreply.github.com>
2022-12-15 15:30:39 +0300

    Add files via upload

---
+++ Калькулятор 24/Main.js
@@ -0,0 +1,138 @@
+var ele;
+     function ClearAll()
+    {
+     ele = document.getElementById("result");
+     ele.value = "";
+     }
+	 
+	 
+   function ClearLastOne()
+  {
+    ele = document.getElementById("result");
+    var res = ele.value;
+	var rend = res.length - 1;
+	res = ele.value.slice(0,rend);
+	ele.value = res;
+   }
+
+   function inputExp(symbol) 
+        {
+            ele = document.getElementById("result");
+            ele.value += symbol;
+        }
+		
+
+   
+   
+   
+   
+   
+   
+   function znak(symbol)
+    {
+
+	    ele = document.getElementById("result");
+		var elestr = ele.value;
+
+		const znakarr = new Array ('+','-','^','*','÷');
+
+		let b = new Boolean(check()); 
+		//alert ("значение б после функции= "+b)        
+		let c= new Boolean();
+
+
+      for (let i = 0; i < znakarr.length; i++)
+	  {		
+		 var lstele = elestr.lastIndexOf(znakarr[i]);
+
+		if (lstele == elestr.length - 1)
+	    {
+		  //alert ("true1") 			
+		  c = true;
+          break;	
+
+	    }
+		
+	    else 
+	   {
+
+		   if (b == true)
+		   {
+			//  alert("true2")			   
+			  c = true;
+			  break;
+		    }
+		    else 
+		   {
+			  //alert("false1")
+			  c = false;			  
+			  break;
+
+		    }
+	    } 
+      }
+	   // alert (c+'=c')
+	       if (c == true)
+		 {
+		//	 alert ('replace')
+			ClearLastOne();
+ 			inputExp(symbol);
+		 }
+		 else 
+		 {
+			// alert ('new')
+			 inputExp(symbol);
+		 }
+     }
+
+
+
+   
+ 		function check()
+ {
+	    var cikl = 0;
+		const znakarr = new Array ('+','-','^','*','÷');	
+	    ele = document.getElementById("result");
+		var elestr = ele.value;
+				
+        for (let j = 0; j < znakarr.length; j++)
+	    {		
+		 var lstele = elestr.lastIndexOf(znakarr[j]);
+		 //alert (lstele+" = "+(elestr.length))
+
+		 if (lstele == elestr.length - 1)
+	    {
+		//	alert ('знак совпадает')
+         var b = 1;
+		 break;
+	    }
+	    else 
+	    { 
+	//alert ('знак не совпадает, проверка следующего')
+			var b = 0;	
+	    }
+       }
+	   		if (b==1)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	  // alert ("значение функции чек = "+b);
+
+ }
+ 
+ 
+ function equal()
+ {
+	 var expresion = document.getElementById("result");
+	 var exp = expresion.value; 
+     const term = exp.split(/\+|\-|\*|\÷|\^/);
+	 alert (term [0])
+	 const exparr = exp.split('');
+	 
+	 
+ }
+ 
